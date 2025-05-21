9fb3b4421e4e93bf4106db8e1021c81b8dcf0910
NguyenDuyDong862003 <21130318@st.hcmuaf.edu.vn>
2025-05-21 10:35:33 +0700

    sửa lại chính tả OrderDetail

--- src/main/java/com/TTLTTBDD/server/repositories/OderDetailRepository.java
+++ src/main/java/com/TTLTTBDD/server/repositories/OderDetailRepository.java
@@ -1,16 +1,16 @@
 package com.TTLTTBDD.server.repositories;
 
-import com.TTLTTBDD.server.models.entity.OderDetail;
+import com.TTLTTBDD.server.models.entity.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface OderDetailRepository extends JpaRepository<OderDetail, Integer> {
-    List<OderDetail> findByIdOder_Id(int orderId);
+public interface OderDetailRepository extends JpaRepository<OrderDetail, Integer> {
+    List<OrderDetail> findByIdOder_Id(int orderId);
 
-    @Query("SELECT COUNT(od) > 0 FROM OderDetail od " +
+    @Query("SELECT COUNT(od) > 0 FROM OrderDetail od " +
             "JOIN od.idOder o " +
             "WHERE o.idUser.id = :userId AND od.idProduct.id = :productId " +
             "AND o.idStatus.name = 'COMPLETED'")
