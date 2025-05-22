343748cacaf466ea7f1bbbe2b6f49a9a2968bf22
NguyenDuyDong862003 <21130318@st.hcmuaf.edu.vn>
2025-05-22 21:25:44 +0700

    Thêm cross 3000

--- src/main/java/com/TTLTTBDD/server/controllers/ReviewController.java
+++ src/main/java/com/TTLTTBDD/server/controllers/ReviewController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000"})
 @RequestMapping("/api/reviews")
 public class ReviewController {
     private final ReviewService reviewService;
